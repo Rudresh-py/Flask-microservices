f7f583f87ea681e03371a213add8f3c4b80bafb0
Rudreshcg <rudresha.gangaswamaiah@softsuave.com>
2023-04-05 21:08:27 +0530

    Readme.md

--- user/app.py
+++ user/app.py
@@ -64,26 +64,6 @@ class RegisterApi(Resource):
         })
 
 
-# api.add_resource(RegisterApi, '/register')
-
-
-#
-#     return jsonify({'id': user.id, 'username': user.username}), 201
-
-# @app.route('/register', methods=['POST'])
-# def create_user():
-#     username = request.json.get('username')
-#     password = request.json.get('password')
-#     if not username or not password:
-#         return jsonify({'error': 'Username and password are required'}), 400
-#
-#     user = User(username=username, password=generate_password_hash(password))
-#     db.session.add(user)
-#     db.session.commit()
-#
-#     return jsonify({'id': user.id, 'username': user.username}), 201
-
-
 class LoginApi(Resource):
     def post(self):
         # username = request.json.get('username')
@@ -109,25 +89,6 @@ class LoginApi(Resource):
         })
 
 
-# api.add_resource(LoginApi, '/login')
-
-
-#
-# @app.route('/login', methods=['POST'])
-# def login():
-#     username = request.json.get('username')
-#     password = request.json.get('password')
-#     if not username or not password:
-#         return jsonify({'error': 'Username and password are required'}), 400
-#
-#     user = User.query.filter_by(username=username).first()
-#     if not user or not check_password_hash(user.password, password):
-#         return jsonify({'error': 'Invalid username or password'}), 401
-#
-#     session['user_id'] = user.id
-#     return jsonify({'id': user.id, 'username': user.username}), 200
-
-
 class LogoutApi(Resource):
     def get(self):
         data = request.get_json()
@@ -143,18 +104,6 @@ class LogoutApi(Resource):
             })
 
 
-# api.add_resource(LogoutApi, '/logout')
-
-
-# @app.route('/logout')
-# def logout():
-#     if 'user_id' in session:
-#         session.pop('user_id', None)
-#         return jsonify({'message': 'Logged out successfully'}), 200
-#     else:
-#         return jsonify({'error': 'User is not authenticated'}), 401
-
-
 class CreateProduct(Resource):
     def post(self):
         data = request.get_json()
@@ -167,21 +116,6 @@ class CreateProduct(Resource):
         return jsonify({
             'status': 'success your product is created'
         })
-
-
-# api.add_resource(CreateProduct, '/products')
-
-
-# @app.route('/products', methods=['POST'])
-# def create_product():
-#     data = request.get_json()
-#     id = data['id']
-#     title = data['title']
-#     image = data['image']
-#     new_product = Products(id=id, title=title, image=image)
-#     db.session.add(new_product)
-#     db.session.commit()
-#     return jsonify({'status': 'success'})
 
 
 class LikeProducts(Resource):
