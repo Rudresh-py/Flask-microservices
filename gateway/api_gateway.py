6a9668052f8246b8c87ed6d533c53da0100b1fd8
Rudreshcg <rudresha.gangaswamaiah@softsuave.com>
2023-04-05 15:06:55 +0530

    created Api Gateway for manage communication between microservices

--- gateway/api_gateway.py
+++ gateway/api_gateway.py
@@ -1,7 +1,11 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, session
 import requests
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'mygatewaysecretkey'
+
+
+user_service_url = 'http://127.0.0.1:5002'
 
 
 @app.route('/')
@@ -9,16 +13,69 @@ def home():
     return 'API Gateway'
 
 
-@app.route('/users')
-def get_users():
-    response = requests.get('http://localhost:5001')
-    return jsonify(response.text)
-
-
-@app.route('/products')
+@app.route('/products', methods=['GET'])
 def get_products():
-    response = requests.get('http://localhost:5002')
-    return jsonify(response.text)
+    response = requests.get('http://localhost:5001/products')
+    data = response.json()
+    return jsonify(data)
+
+
+@app.route('/products', methods=['POST'])
+def create_products():
+    # data = requests.json
+    new_product = {
+        "title": request.json.get('title'),
+        "image": request.json.get('image'),
+        "likes": request.json.get('likes')
+    }
+    response = requests.post('http://localhost:5001/products', json=new_product)
+    data = response.json()
+    return jsonify(data)
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    new_user = {
+        "username": request.json.get('username'),
+        "password": request.json.get('password')
+    }
+
+    response = requests.post(f'{user_service_url}/register', json=new_user)
+    data = response.json()
+    return jsonify(data)
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    login_user = {
+        "username": request.json.get('username'),
+        "password": request.json.get('password')
+    }
+    response = requests.post(f'{user_service_url}/login', json=login_user)
+    data = response.json()
+    session['user_id'] = data['id']
+    return jsonify(data)
+
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    try:
+        user_id = {
+            "user_id": session["user_id"]
+            }
+        response = requests.get(f'{user_service_url}/logout', json=user_id)
+        session.pop('user_id', None)
+        data = response.json()
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({'error': 'User is not authenticated'})
+
+
+@app.route('/products/<int:id>/like', methods=['GET'])
+def like_products(id):
+    response = requests.get(f'{user_service_url}/products/{id}/like')
+    data = response.json()
+    return jsonify(data)
 
 
 if __name__ == "__main__":
